0226a4fee3117f6e49c29dd42a2886e88b0950fb
Sandra <sandrajerotich1@gmail.com>
2024-08-27 13:48:42 +0300

    Passport

--- app.js
+++ app.js
@@ -11,17 +11,15 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
-const  url = 'mongodb://localhost:27017/nucampsites';
-const connect = mongoose.connect(url,{
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true  
-});
-connect.then(()=> console.log('Connected correctly to server'),
-err => console.log(err));
+const url = 'mongodb://localhost:27017/nucampsites';
+const connect = mongoose.connect(url);
+
+connect.then(() => console.log('Connected correctly to server'),
+    err => console.log(err));
 
 var app = express();
 
@@ -32,7 +30,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(session({
   name: 'session-id',
@@ -42,47 +40,47 @@ app.use(session({
   store: new FileStore()
 }));
 
-function auth(req, res, next) {
-  console.log(req.session);
+// Initialize passport and manage sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
-  if (!req.session.user) {
+// Route handling for index and users
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Authentication Middleware
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
       const err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err);
   } else {
-      if (req.session.user === 'authenticated') {
-          return next();
-      } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      }
+      return next();
   }
 }
 
+// Protect routes with auth middleware
 app.use(auth);
 
-
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Protected routes
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
-// catch 404 and forward to error handler
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
